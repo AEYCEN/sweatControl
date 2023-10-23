15a54965532e6e5b836f675124c9eeceda2366ac
a.campbell-smith <alexanderc50@gmail.com>
2023-10-23 08:11:35 +0200

    ficken123

--- model.py
+++ model.py
@@ -1,8 +1,5 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-
 
 class HeaterModel:
     def __init__(self):
@@ -10,29 +7,22 @@ class HeaterModel:
         # Du solltest deine eigenen Daten für das Training verwenden
         data = {
             'external_temperature': [0, 5, 10, 15, 20, 25, 30],
-            'heater_temperature': [20, 22, 25, 28, 30, 32, 35]
+            'selected_heater_temperature': [15, 16, 17, 18, 19, 20, 21],
+            'target_vorlauftemperatur': [40, 42, 44, 45, 46, 47, 48]
         }
 
         # Daten in ein Pandas DataFrame laden
         df = pd.DataFrame(data)
 
         # Feature (X) und Target (y) Daten vorbereiten
-        X = df[['external_temperature']]
-        y = df['heater_temperature']
-
-        # Daten in Trainings- und Testsets aufteilen
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        X = df[['external_temperature', 'selected_heater_temperature']]
+        y = df['target_vorlauftemperatur']
 
         # Lineare Regression initialisieren und trainieren
         self.model = LinearRegression()
-        self.model.fit(X_train, y_train)
+        self.model.fit(X, y)
 
-        # Modellgenauigkeit auf dem Testset überprüfen
-        y_pred = self.model.predict(X_test)
-        mse = mean_squared_error(y_test, y_pred)
-        print(f'Mean Squared Error on Test Data: {mse}')
-
-    def predict_temperature(self, external_temperature):
+    def predict_vorlauftemperatur(self, external_temperature, selected_heater_temperature):
         # Vorhersage treffen
-        predicted_temperature = self.model.predict([[external_temperature]])
-        return predicted_temperature[0]
+        predicted_vorlauftemperatur = self.model.predict([[external_temperature, selected_heater_temperature]])
+        return predicted_vorlauftemperatur[0]
