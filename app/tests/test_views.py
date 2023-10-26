300273a34bc42e68642050541e181313ccd8fc19
AEYCEN <114836625+AEYCEN@users.noreply.github.com>
2023-10-26 22:53:15 +0200

    testing, attempt 3

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -1,3 +1,14 @@
+import pytest
+from app import create_app
+
+
+@pytest.fixture
+def client():
+    app = create_app()
+    with app.test_client() as client:
+        yield client
+
+
 def test_app(client):
     resp = client.get("/")
 
