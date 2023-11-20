f8e943cb97b7892b62aa04eb7fcfc67e62bc9885
a.campbell-smith <alexanderc50@gmail.com>
2023-11-20 13:36:19 +0100

    Ich liebe euch

--- app/HeaterController.py
+++ app/HeaterController.py
@@ -31,25 +31,20 @@ class HeaterController:
         return self.room_temperature
 
     def get_heater_temperature(self):
-        return self.model.predict_heater_temperature(self.get_external_temperature(), self.get_room_temperature(),
+        return self.model.predict_heater_temperature(round(self.get_external_temperature(), 2), self.get_room_temperature(),
                                                      self.min_heater_temperature, self.max_heater_temperature)
 
     def update_room_temperature(self, new_temperature):
         self.room_temperature = new_temperature
 
     def get_data_diagram(self):
-        x = [-20, -18, -16, -14, -12, -10, -8, -6, -4, -2, 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+        x = [20, 18, 16, 14, 12, 10, 8, 6, 4, 2, 0, -2, -4, -6, -8, -10, -12, -14, -16, -18, -20]
         predicted_values = []
-
-        heater_temperature = self.min_heater_temperature
-
-        if heater_temperature < self.room_temperature:
-            heater_temperature = self.room_temperature
 
         for temperature_aussen in x:
             predicted_temperature = self.model.predict_heater_temperature(temperature_aussen,
                                                                           self.get_room_temperature(),
-                                                                          heater_temperature,
+                                                                          self.min_heater_temperature,
                                                                           self.max_heater_temperature)
             predicted_values.append(predicted_temperature)
 
@@ -61,12 +56,3 @@ class HeaterController:
     def get_heater_variant(self):
         return self.heater_variant
 
-
-if __name__ == "__main__":
-    controller = HeaterController()
-    temperature = controller.get_external_temperature()
-    city = get_current_city()
-    if temperature:
-        print(f"{city} - {temperature}°C")
-    else:
-        print("Failed to fetch temperature!")
