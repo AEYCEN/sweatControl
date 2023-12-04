ff2a21ffa1c4682ec694c590971f2d2bf917ee76
AEYCEN <sucram.mra@gmx.de>
2023-12-04 14:17:52 +0100

    refactored nav menu to single toggle button

--- app/static/js/navMenuChanger.js
+++ app/static/js/navMenuChanger.js
@@ -1,18 +1,21 @@
-function activateMenu(menuId) {
-    const menus = document.querySelectorAll('#sc-controls-menu, #sc-weather-menu, #sc-settings-menu');
-    menus.forEach(menu => {
-        if (menu.id === menuId) {
-            menu.style.display = 'grid';
-        } else {
-            menu.style.display = 'none';
-        }
-    });
+function toggleMenu() {
+    const button = document.querySelector('.sc-nav-button');
+    const menu1 = document.querySelector('#sc-controls-menu');
+    const menu2 = document.querySelector('#sc-settings-menu');
+    const sign1 = document.querySelector('#sc-settingstogglesign1');
+    const sign2 = document.querySelector('#sc-settingstogglesign2');
 
-    const buttons = document.querySelectorAll('.sc-nav-button');
-    buttons.forEach(button => {
+    if (button.classList.contains('sc-button-active')) {
+        menu2.style.display = 'none';
+        menu1.style.display = 'grid';
+        sign1.style.display = 'flex';
+        sign2.style.display = 'none';
         button.classList.remove('sc-button-active');
-    });
-
-    const activeButton = document.querySelector(`[onclick="activateMenu('${menuId}')"]`);
-    activeButton.classList.add('sc-button-active');
+    } else {
+        menu1.style.display = 'none';
+        menu2.style.display = 'grid';
+        sign1.style.display = 'none';
+        sign2.style.display = 'flex';
+        button.classList.add('sc-button-active');
+    }
 }
